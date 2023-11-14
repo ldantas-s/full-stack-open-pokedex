a5402424d5072b27980639c870dabcba2db943f8
ldantas-s <lucianostudant@gmail.com>
2023-11-14 17:42:17 -0300

    feat: add cypress to make e2e tests

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+// eslint-disable-next-line no-undef
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  },
+})
