8505ea84176903553b8cb2ee4b01f08c06f4ca4e
ldantas-s <lucianostudant@gmail.com>
2023-11-14 21:10:50 -0300

    test: add one more test using cypress getting inside of a page

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('getting inside of a charmander page', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('[href="/pokemon/charmander"]').click()
+    cy.contains('.pokemon-ability-name', 'blaze')
+  })
 })
